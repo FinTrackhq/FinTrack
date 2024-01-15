e1596e4f9b4d6f2fc884cefa5fde27a4fce825f0
Zhan <62433092+yahannes@users.noreply.github.com>
2024-01-15 23:25:43 +0300

    adding debug bar

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -4,8 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Company extends Model
 {
-    use HasFactory;
+
+
+    use HasFactory, SoftDeletes;
 }
