80bfb49706ce8b041d1b629d7c72306d35014f3b
Zhan <62433092+yahannes@users.noreply.github.com>
2024-01-20 11:39:10 +0300

    + Company Statement

---
+++ config/filament-email.php
@@ -0,0 +1,18 @@
+<?php
+
+// config for RickDBCN/FilamentEmail
+use RickDBCN\FilamentEmail\Filament\Resources\EmailResource;
+
+return [
+
+    'resource' => [
+        'class' => EmailResource::class,
+        'group' => null,
+        'sort' => null,
+        'default_sort_column' => 'created_at',
+        'default_sort_direction' => 'desc',
+    ],
+
+    'keep_email_for_days' => 60,
+    'label' => null,
+];
