b71c9ceaaf1071a6ad710640d2e28ab1eebf083c
Zhan <62433092+yahannes@users.noreply.github.com>
2024-02-26 04:13:05 +0300

    Yandex webmaster

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -4,6 +4,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <title>{{config('app.name')}}</title>
 <script src="https://cdn.tailwindcss.com"></script>
+<meta name="yandex-verification" content="040c73c3b4e413b3" />
 <script>
     tailwind.config = {
         theme: {
