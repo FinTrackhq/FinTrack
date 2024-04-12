17bbf6d1dd41da4f846c18cb418b84bb6de992b0
yahannes <crashermoney0@gmail.com>
2024-04-12 23:58:09 +0300

    Error pages restyled

--- resources/views/errors/minimal.blade.php
+++ resources/views/errors/minimal.blade.php
@@ -4,12 +4,15 @@
        @include('components.head')
     </head>
     <body class="antialiased">
-    <div class="grid h-screen place-content-center bg-white px-4">
-        <div class="text-center">
-            <img src="{{asset('file/image/logo/svg/logo.svg')}}" class="object-cover items-center w-auto h-32" alt="FinTrack">
-            <h1 class="mt-6 text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">@yield('code')</h1>
-
-            <p class="mt-4 text-gray-500"> @yield('message')</p>
+    <div class="hero min-h-screen" style="background-image: url('{{asset('file/image/screenshots/bg.jpg')}}');">
+        <div class="hero-overlay bg-opacity-30"></div>
+        <div class="hero-content text-center text-neutral-content">
+            <div class="max-w-md">
+                <h1 class="mb-5 text-5xl font-bold">@yield('code')</h1>
+                <p class="mb-5">@yield('message')</p>
+                <a class="btn btn-primary" href="{{url()->previous()}}"> <x-heroicon-o-arrow-long-left  class="w-4"/>Back</a>
+                <a class="btn btn-info" href="https://t.me/ya_hannes"> <x-heroicon-s-user class="w-4"/>Support</a>
+            </div>
         </div>
     </div>
     </body>
