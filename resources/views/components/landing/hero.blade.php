168941e75c7a38cb24f2ba33238a1d0e9321e956
yahannes <crashermoney0@gmail.com>
2024-04-13 00:12:26 +0300

    Landing mobile version fixed and hero component

---
+++ resources/views/components/landing/hero.blade.php
@@ -0,0 +1,10 @@
+<div class="hero min-h-screen" style="background-image: url('{{asset('file/image/screenshots/bg.jpg')}}');">
+    <div class="hero-overlay bg-opacity-60"></div>
+    <div class="hero-content text-center text-neutral-content">
+        <div class="max-w-md">
+            <h1 class="mb-5 text-5xl font-bold">FinTrack</h1>
+            <p class="mb-5">The most functional, beautiful, user-friendly open source tool!</p>
+            <a href="{{route('filament.dashboard.auth.login')}}" class="btn btn-primary">Get Started</a>
+        </div>
+    </div>
+</div>
--- resources/views/main/main.blade.php
+++ resources/views/main/main.blade.php
@@ -1,30 +1,21 @@
 @extends('components.layouts.main')
 @section('main')
-    <div class="hero min-h-screen" style="background-image: url('{{asset('file/image/screenshots/bg.jpg')}}');">
-        <div class="hero-overlay bg-opacity-60"></div>
-        <div class="hero-content text-center text-neutral-content">
-            <div class="max-w-md">
-                <h1 class="mb-5 text-5xl font-bold">FinTrack</h1>
-                <p class="mb-5">The most functional, beautiful, user-friendly open source tool!</p>
-                <a href="{{route('filament.dashboard.auth.login')}}" class="btn btn-primary">Get Started</a>
-            </div>
-        </div>
-    </div>
-    <div class="stats shadow flex py-8">
+   <x-landing.hero></x-landing.hero>
+    <div class="stats shadow flex py-8 text-sm lg:text-lg">
         <div class="stat place-items-center">
             <div class="stat-title"><a href="https://github.com/FinTrackhq/FinTrack" class="link">GitHub Stars</a></div>
 
-            <div class="stat-value">{{$response}}</div>
+            <div class="stat-value text-2xl lg:text-4xl">{{$response}}</div>
         </div>
 
         <div class="stat place-items-center">
             <div class="stat-title"><a href="https://github.com/FinTrackhq/FinTrack/graphs" class="link">Clones</a></div>
-            <div class="stat-value text-value">100+</div>
+            <div class="stat-value text-value text-2xl lg:text-4xl">100+</div>
         </div>
 
         <div class="stat place-items-center">
             <div class="stat-title"><a href="{{route('Home.Page')}}" class="link">Created companies</a></div>
-            <div class="stat-value">20+</div>
+            <div class="stat-value text-2xl lg:text-4xl">20+</div>
         </div>
     </div>
     <x-landing.project-device-support></x-landing.project-device-support>
