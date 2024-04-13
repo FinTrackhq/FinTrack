40535b244c5825228d8be1e3d2ddd9e2dfbfaeab
Zhan Grigoryan <62433092+yahannes@users.noreply.github.com>
2024-04-13 12:13:27 +0300

    The github api was temporarily cut out

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -10,7 +10,6 @@ class HomeController extends Controller
 {
     public function HomePage()
     {
-        $response = Http::get('https://api.github.com/repos/fintrackhq/fintrack')['stargazers_count'];
         return view('main.main');
     }
 
