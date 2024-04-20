1a759f7ec4b0f8cd20ef736d96231644b9a6a344
yahannes <crashermoney0@gmail.com>
2024-04-20 14:13:54 +0300

    bug fix

--- routes/web.php
+++ routes/web.php
@@ -10,16 +10,13 @@ use Spatie\Health\Http\Controllers\HealthCheckResultsController;
 | Web Routes
 |--------------------------------------------------------------------------
 */
-
+Route::domain('status.fintrack.space')->group(function () {
+    Route::get('/', HealthCheckResultsController::class);
+});
 Route::controller(HomeController::class)->group(function (){
     Route::get('/', 'HomePage')->name('Home.Page');
     Route::post('/', 'LogOut')->name('LogOut');
 });
 Route::controller(FeedbackController::class)->prefix('feedback')->group(function (){
     Route::post('/create', 'create')->name('feedback.create');
-});
-
-
-Route::domain('status.fintrack.space')->group(function () {
-    Route::get('/up', HealthCheckResultsController::class);
 });
